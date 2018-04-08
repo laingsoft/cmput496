085f88a278394daec60b89190f00ffa6aaa939df
Alexander Ozero <ozero@ualberta.ca>
2018-04-07 20:00:00 -0600

    Add genmove override

--- assignment4/Go5/gtp_connection_go5.py
+++ assignment4/Go5/gtp_connection_go5.py
@@ -32,6 +32,7 @@ class GtpConnection(gtp_connection.GtpConnection):
         gtp_connection.GtpConnection.__init__(self, go_engine, board, outfile, debug_mode)
         
         self.commands["prior_knowledge"] = self.prior_knowledge_cmd
+        self.commands["genmove"] = self.genmove_cmd
     
     def prior_knowledge_cmd(self, args):
         moves = []
@@ -64,4 +65,43 @@ class GtpConnection(gtp_connection.GtpConnection):
             response += str(GoBoardUtil.sorted_point_string([i.move], self.board.NS)) + " " + str(i.wins)+ " " + str(i.sim) + " "
         
         self.respond(response)
+        
+    def genmove_cmd(self, args):
+        """
+        generate a move for the specified color
+
+        Arguments
+        ---------
+        args[0] : {'b','w'}
+            the color to generate a move for
+            it gets converted to  Black --> 1 White --> 2
+            color : {0,1}
+            board_color : {'b','w'}
+        """
+        try:
+            board_color = args[0].lower()
+            color = GoBoardUtil.color_to_int(board_color)
+            self.debug_msg("Board:\n{}\nko: {}\n".format(str(self.board.get_twoD_board()),
+                                                          self.board.ko_constraint))
+            move = self.go_engine.get_move(self.board, color)
+            if move is None:
+                self.respond("pass")
+                return
+
+            if not self.board.check_legal(move, color):
+                move = self.board._point_to_coord(move)
+                board_move = GoBoardUtil.format_point(move)
+                self.respond("Illegal move: {}".format(board_move))
+                raise RuntimeError("Illegal move given by engine")
+
+            # move is legal; play it
+            self.board.move(move,color)
+
+            self.debug_msg("Move: {}\nBoard: \n{}\n".format(move, str(self.board.get_twoD_board())))
+            move = self.board._point_to_coord(move)
+            board_move = GoBoardUtil.format_point(move)
+            self.respond(board_move)
+        except Exception as e:
+            self.respond('Error: {}'.format(str(e)))
+            raise
         
